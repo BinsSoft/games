99a32c7c3b9956804d35642084775296fc4ff4a8
Tonmoy Nandy <tonmoy.nandy@gmail.com>
2018-06-23 02:30:02 +0530

    change the inner logic

--- crossgame/script.js
+++ crossgame/script.js
@@ -1,106 +1,43 @@
-class CorssGame {
-    
-	constructor(selector){
-		this.selector = $(selector);
-		this.row = [1,4,7];
+function searchForArray(haystack, needle){
+	var i, j, current;
+	for(i = 0; i < haystack.length; ++i){
+		if(needle.length === haystack[i].length){
+		  current = haystack[i];
+		  for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
+		  if(j === needle.length)
+		    return i;
+		}
+	}
+	return -1;
+}
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 10; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+var playerList = [];
+var totalHit = 0;
+
+class Board {
+	constructor() {
 		this.winCombinations = [
-				[1,2,3],
-				[4,5,6],
-				[7,8,9],
-				[1,4,7],
-				[2,5,8],
-				[3,6,9],
-				[1,5,9],
-				[3,5,7]	
-			];
-		this.players = [
-			{
-				name : '',
-				result : []
-			},
-			{
-				name : '',
-				result : []
-			},
+			[1,2,3],
+			[4,5,6],
+			[7,8,9],
+			[1,4,7],
+			[2,5,8],
+			[3,6,9],
+			[1,5,9],
+			[3,5,7]	
 		];
-		this.playerTurn = 0;
-		this.totalClick = 0;
-		this.init()
 	}
-	init(){
-
-		var playerContainer = $("<div/>")
-							.addClass('player-conatiner')
-							.appendTo(this.selector);
-		let playerDisplayContainer = $("<div/>").addClass('player-display-container').appendTo(playerContainer)
-		var playContainer = $("<div/>")
-							.addClass('play-conatiner hidden')
-							.attr({
-								'data-turn' : 0,
-								'data-click-count' : 0
-							})
-							.appendTo(this.selector);
-		let playerForm = $("<div/>")
-						.addClass('player-form')
-						.appendTo(playerContainer);
-		let nameInput = $("<input />")
-		.attr({
-			type : 'text',
-			placeholder : 'Enter Name'
-		})
-		.appendTo(playerForm)
-		$("<input />")
-		.attr({
-			type : 'button',
-			value : 'Enter'
-		})
-		.bind('click',()=>{
-			$(this.players).each((i,player)=>{
-				let attrName = playerDisplayContainer.find('p:eq('+i+')').attr('data-name');
-				this.players[i].name = (attrName != undefined)? attrName :'';
-			})
-			let blankIndex = this.players.findIndex((p)=>{
-				return p.name == '';
-			})
-			this.players[blankIndex].name = nameInput.val()
-
-			socket.emit('players', this.players);
-
-			playerForm.addClass('hidden');
-			playContainer.removeClass('hidden');
-			this.appendBoard(playContainer);
-		})
-		.appendTo(playerForm)
-
-		
-		socket.on('game-players', function(data){
-			playerDisplayContainer.empty();
-			for (let p in data) {
-				let name  = 'waiting ...';
-				let attrName  = '';
-				if (data[p].name != '') {
-					name  = attrName = data[p].name;
-				}
-				$("<p/>")
-				.attr({
-					"data-name" : attrName
-				})
-				.html("<strong>Player :</strong>"+name)
-				.appendTo(playerDisplayContainer);
-			}
-			let blankIndex = data.findIndex((p)=>{
-				return p.name == '';
-			})
-			if(blankIndex == -1) {
-				$("<p/>")
-				.html('Game will start shortly')
-				.appendTo(playerDisplayContainer);
-			}
-		});
-	}
-
 	appendBoard(playContainer)
 	{
+		var rows = [1,4,7];
 		var titleContainer = $("<div/>")
 					.addClass('title-container')
 					.appendTo(playContainer);
@@ -120,7 +57,7 @@ class CorssGame {
 				.addClass('row')
 				.appendTo(board);
 			for (let c=0; c<3; c++) {
-				let cellValue = this.row[r]+c;
+				let cellValue = rows[r]+c;
 				$("<div/>")
 				.attr({
 					'data-value' : cellValue,
@@ -128,39 +65,35 @@ class CorssGame {
 				})
 				.addClass('cell')
 				.bind('click',()=>{
-					if ($("div.cell[data-value="+cellValue+"]").attr('data-click') == 'true') {
-						this.playerTurn = playContainer.attr('data-turn');
-						this.totalClick = playContainer.attr('data-click-count');
-						this.players[this.playerTurn].result.push(cellValue);
-
-						
-						this.playerTurn = (this.playerTurn == 0) ? 1:0;
-						turnContainer.text('Now '+this.players[this.playerTurn].name+' Turn');
-						this.totalClick ++;
+					let turn = playContainer.attr('data-turn');
+					let currentUser = playerList[turn];
+					let currentKey = playContainer.attr('data-current-key');
+					let totalCell = playContainer.find('.cell').length;
+					if (
+						(currentKey == currentUser['id']) && // check click by  current user or not
+					 	(totalCell > totalHit) &&  // as per game total hit will be 9, check max no of hit
+					 	($("div.cell[data-value="+cellValue+"]").attr('data-click') == 'true') // check the cell is already clicked or not
+					 	) {
+						totalHit ++;
 						socket.emit('players-result', 
 							{
-								turn : this.playerTurn, 
+								turn : turn, 
 								cell : cellValue,
-								totalclick : this.totalClick,
-								result: this.players
+								totalclick : totalHit
 							}
 						);
-						/*
-						
-						
-						
-						this.calculateResult();*/
-						
 					}
+					
 				})
 				.appendTo(row);
 			}
 		}
 		socket.on('game-players-result', (responseData)=>{
-			console.log(responseData);
-			let html = '';
+			let turn = responseData.turn;
 			let cellValue = responseData.cell;
-			let turn = (responseData.turn == 0) ? 1:0;
+			let hitUser = playerList[turn];
+			playerList[turn].result.push(cellValue);
+			let html = '';
 			if (turn == 0) {
 				html = '&#x2716';
 			} else {
@@ -169,43 +102,117 @@ class CorssGame {
 			$("div.cell[data-value="+cellValue+"]")
 			.html('<div class="cell-content">'+html+'</div>')
 			.attr('data-click', 'false');
-
-			playContainer.attr({
-				'data-turn' : responseData.turn,
-				'data-click-count' : responseData.totalclick
-			});
+			turn = (turn == 0)? 1:0;
+			let result = this.calculateResult();
+			if (result == false) { // result not declared
+				alert(playerList[turn].name + "'s turn now");
+				playContainer.attr({
+					'data-turn' : turn
+				});
+			}
+			
 		})
 	}
 	calculateResult()
 	{
-		if(this.totalClick < $("div.cell").length ) {
-			let player1Result = this.players[0].result;
-			let player2Result = this.players[1].result;
+		if(totalHit <= $("div.cell").length ) {
+			let player1Result = playerList[0].result;
+			let player2Result = playerList[1].result;
 			player1Result.sort();
 			player2Result.sort();
 
-			if (this.searchForArray(this.winCombinations, player1Result) > -1) {
-				this.resultContainer.text( this.players[0].name + ' won the game' );
-			} else if (this.searchForArray(this.winCombinations, player2Result) > -1) {
-				this.resultContainer.text( this.players[1].name + ' won the game' );
+			if (searchForArray(this.winCombinations, player1Result) > -1) {
+				alert( playerList[0].name + ' won the game' );
+				return true;
+			} else if (searchForArray(this.winCombinations, player2Result) > -1) {
+				alert( playerList[1].name + ' won the game' );
+				return true;
 			}
 
 		} else {
-			this.resultContainer.text( 'Match is drawn' );
+			alert( 'Match is drawn' );
+			return true;
 		}
+		return false;
 	}
-	searchForArray(haystack, needle){
-		var i, j, current;
-		for(i = 0; i < haystack.length; ++i){
-			if(needle.length === haystack[i].length){
-			  current = haystack[i];
-			  for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
-			  if(j === needle.length)
-			    return i;
+	
+}
+class CorssGame {
+    
+	constructor(selector){
+		this.selector = $(selector);
+		
+		
+		this.playerTurn = 0;
+		this.currentUserKey = makeid();
+
+		this.init()
+	}
+	init(){
+
+		var playerContainer = $("<div/>")
+							.addClass('player-conatiner')
+							.appendTo(this.selector);
+		let playerDisplayContainer = $("<div/>").addClass('player-display-container').appendTo(playerContainer)
+		let turnContainer = $("<div/>").addClass('turn-container').appendTo(playerContainer)
+		var playContainer = $("<div/>")
+							.addClass('play-conatiner hidden')
+							.attr({
+								'data-turn' : 0,
+								'data-click-count' : 0,
+								'data-current-key' : this.currentUserKey,
+							})
+							.appendTo(this.selector);
+		let playerForm = $("<div/>")
+						.addClass('player-form')
+						.appendTo(playerContainer);
+		let nameInput = $("<input />")
+		.attr({
+			type : 'text',
+			placeholder : 'Enter Name'
+		})
+		.appendTo(playerForm)
+		$("<input />")
+		.attr({
+			type : 'button',
+			value : 'Enter'
+		})
+		.bind('click',()=>{
+			let currentUser = {
+				name : nameInput.val(),
+				id : this.currentUserKey,
+				result : []
+			};
+			socket.emit('players', currentUser);
+			playerForm.addClass('hidden');
+		})
+		.appendTo(playerForm)
+
+		
+		socket.on('game-players', function(data){
+			let turn = playContainer.attr('data-turn');
+			playerDisplayContainer.empty();
+			playerList.push(data);
+			for (let p of playerList) {
+				$("<p/>")
+				.attr({
+					"data-name" : p.name
+				})
+				.html("<strong>Player :</strong>"+p.name)
+				.appendTo(playerDisplayContainer);
 			}
-		}
-		return -1;
+			if (playerList.length == 2) {
+				let board =new Board;
+				board.appendBoard(playContainer)
+				playContainer.removeClass('hidden');
+				alert("Now Game will be started");
+				alert(playerList[turn].name + "'s turn now");
+			}
+		});
 	}
+
+	
+	
 }
 
 $(()=>{
