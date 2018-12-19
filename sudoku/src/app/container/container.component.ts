3ada19d55a363384ad3885f8b1bc13c2b0ea4f7d
Tonmoy Nandy <tonmoy@matrixnmedia.com>
2018-12-19 20:02:45 +0530

    work game bolck development

--- sudoku/src/app/container/container.component.ts
+++ sudoku/src/app/container/container.component.ts
@@ -11,7 +11,18 @@ export class ContainerComponent implements OnInit {
 	totalCell: number = 0;
 	totalBlock: number = 0;
 	cellWidth: number = 50;
-	cellGenerateOrder: Array<any> = [5,1,9,3, 7,2,4,6];
+	cellGenerateOrder: Array<any> = [5,1,9,3, 7,2,4,6, 8];
+	allCells: any = {
+		"1" : [1,2,3,4,5,6,7,8,9],
+		"2" : [1,2,3,4,5,6,7,8,9],
+		"3" : [1,2,3,4,5,6,7,8,9],
+		"4" : [1,2,3,4,5,6,7,8,9],
+		"5" : [1,2,3,4,5,6,7,8,9],
+		"6" : [1,2,3,4,5,6,7,8,9],
+		"7" : [1,2,3,4,5,6,7,8,9],
+		"8" : [1,2,3,4,5,6,7,8,9],
+		"9" : [1,2,3,4,5,6,7,8,9]
+	};
 
 	constructor(public el: ElementRef) { }
 
@@ -70,19 +81,31 @@ export class ContainerComponent implements OnInit {
 	generatecellText() {
 		this.cellGenerateOrder.forEach((block)=>{
 			let midBlockElements = document.querySelectorAll("td[data-block='"+block+"']");
-			let cellArr = [1,2,3,4,5,6,7,8,9];
+			let cellArr = [1,2,3,4,5,6,7,8,9]; // this.allCells[block];
 			midBlockElements.forEach((item:any)=> {
 					
 				let cell = this.getProperCell(cellArr, item);
 				if (cell) {
 					item.setAttribute("data-value", cell);
 					item.innerText = cell;
-					cellArr.splice( cellArr.indexOf(cell) , 1 );
-					console.log(cell, cellArr);
+					this.allCells[block].splice( this.allCells[block].indexOf(cell) , 1 );
 				}
 			});
 		});
-		
+
+		let blankElements = document.querySelectorAll("td:not([data-value])");
+		// console.log(blankElements);
+		console.log(this.allCells);
+		/*blankElements.forEach((item:any)=> {
+			let block = item.dataset.block;
+			let cellArr = this.allCells[block];
+			let cell = this.getProperCell(cellArr, item);
+			if (cell) {
+				item.setAttribute("data-value", cell);
+				item.innerText = cell;
+				this.allCells[block].splice( this.allCells[block].indexOf(cell) , 1 );
+			}
+		});*/
 	}
 
 	getProperCell(cellArr, targetElement) {
@@ -98,8 +121,9 @@ export class ContainerComponent implements OnInit {
 			document.querySelectorAll("td[data-row='"+targetRow+"'][data-value='"+cell+"']").length > 0
 			)
 			) {
-			console.log(cell+' is repeat');
+			
 			cellArr.splice( cellArr.indexOf(cell) , 1 );
+			console.log(cell+' is repeat', targetElement, cellArr);
 			return this.getProperCell(cellArr, targetElement);
 		}
 		return cell;
