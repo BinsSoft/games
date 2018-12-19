27df59d3ae29490b01d56eaed5f6ee9ed2b0b79a
Tonmoy Nandy <tonmoy@matrixnmedia.com>
2018-12-19 19:13:45 +0530

    work game bolck development

--- sudoku/src/app/container/container.component.ts
+++ sudoku/src/app/container/container.component.ts
@@ -11,6 +11,8 @@ export class ContainerComponent implements OnInit {
 	totalCell: number = 0;
 	totalBlock: number = 0;
 	cellWidth: number = 50;
+	cellGenerateOrder: Array<any> = [5,1,9,3, 7,2,4,6];
+
 	constructor(public el: ElementRef) { }
 
 	ngOnInit() {
@@ -18,59 +20,91 @@ export class ContainerComponent implements OnInit {
 		this.totalBlock = this.cell * this.cell;
 		this.totalCell = this.totalBlock * this.totalBlock;
 		this.el.nativeElement.style.display = 'inline-block';
-		this.el.nativeElement.style.width = (this.cellWidth*(this.totalBlock+1)).toString()+'px';
-		/*this.el.nativeElement.appendChild(this.generateCells());
-		this.el.nativeElement.querySelectorAll('td').forEach((item, index)=>{
-			item.setAttribute('data-block', index.toString());
-			item.appendChild( this.generateCells(true, item) );
-		});*/
+		//this.el.nativeElement.style.width = (this.cellWidth*(this.totalBlock+1)).toString()+'px';
 		this.generateCells();
+		this.generatecellText();
 	}
 
 	generateCells() {
 		let row: number = 1;
 		let column: number = 1;
+		let block: number = 1;
+		let blockRowCount: number = 0;
+		let table = document.createElement('table');
+		this.el.nativeElement.appendChild(table);
+		let tr = document.createElement('tr');
+		table.appendChild(tr);
+
 		for (let c = 1; c <= this.totalCell; c++) {
-			let span: any = document.createElement('span');
-			span.style['display'] = 'inline-block';
-			span.style['height'] = this.cellWidth.toString()+'px';
-			span.style['width'] = this.cellWidth.toString()+'px';
-			span.style['border'] = '1px solid #CCC';
-			span.innerText = c.toString();
-			span.setAttribute('data-column',column.toString());
-			span.setAttribute('data-row',row.toString());
+			let td: any = document.createElement('td');
+			// td.innerText = column.toString();
+			td.setAttribute('data-column',column.toString());
+			td.setAttribute('data-row',row.toString());
+
+			let module = c%this.totalBlock;
+			if (module >= 1 && module < 3) {
+				block = 1 + blockRowCount ;
+			} else if (module > 3 && module<6) {
+				block = 2 + blockRowCount ;
+			} else if ((module > 6 && module < 9) || module === 0) {
+				block = 3 + blockRowCount ;
+			}
+			if (c%(this.cell * this.totalBlock) === 0) {
+				blockRowCount += this.cell;	
+			}
+			td.setAttribute('data-block',block.toString());
+			
+			tr.appendChild(td);
 			if (column === this.totalBlock) {
 				column = 1;
 				row += 1;
+				tr = document.createElement('tr');
+				table.appendChild(tr);
 			} else {
 				column += 1;
 			}
-
-			console.log(span);
-			this.el.nativeElement.appendChild(span);
 		}
 		
 	}
 
 	generatecellText() {
-		/*let cellCount = 1;
-		let totalBlocks: number = 0;
-		totalBlocks = this.totalCell * this.totalCell;
-		for (let c = 1; c < 2; c++) {
-			this.el.nativeElement.querySelectorAll('td[data-block]').forEach((block)=>{
-				// block.querySelectorAll('td[data-child]')		
+		this.cellGenerateOrder.forEach((block)=>{
+			let midBlockElements = document.querySelectorAll("td[data-block='"+block+"']");
+			let cellArr = [1,2,3,4,5,6,7,8,9];
+			midBlockElements.forEach((item:any)=> {
+					
+				let cell = this.getProperCell(cellArr, item);
+				if (cell) {
+					item.setAttribute("data-value", cell);
+					item.innerText = cell;
+					cellArr.splice( cellArr.indexOf(cell) , 1 );
+					console.log(cell, cellArr);
+				}
 			});
-		}*/
-		/*this.el.nativeElement.querySelectorAll('td[data-child]').forEach((item)=>{
-			
-			item.innerText = cellCount;
-			if (cellCount >= totalBlocks) {
-				cellCount = 1;
-			} else {
-				cellCount++;
-			}
-		});*/
+		});
+		
 	}
+
+	getProperCell(cellArr, targetElement) {
+		let randormIndex = Math.floor(Math.random()*cellArr.length);
+		let cell = cellArr[  randormIndex ];
+		let targetColumn = targetElement.dataset.column;
+		let targetRow = targetElement.dataset.row;
+		let targetBlock = targetElement.dataset.block;
+		if (cellArr.length > 0 
+			&&
+			(document.querySelectorAll("td[data-column='"+targetColumn+"'][data-value='"+cell+"']").length > 0 
+			||
+			document.querySelectorAll("td[data-row='"+targetRow+"'][data-value='"+cell+"']").length > 0
+			)
+			) {
+			console.log(cell+' is repeat');
+			cellArr.splice( cellArr.indexOf(cell) , 1 );
+			return this.getProperCell(cellArr, targetElement);
+		}
+		return cell;
+	}
+
 
 
 }
